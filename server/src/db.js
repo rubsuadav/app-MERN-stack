f3ab45107beaf23a8e4d3f3f5bda7f6f6d862ab2
rubsuadav <rsuarezdavid@gmail.com>
2023-06-16 02:28:33 +0200

    removing "then" method in promises due to use async/await

--- server/src/db.js
+++ server/src/db.js
@@ -6,37 +6,34 @@ const connection = mongoose.connection;
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URL).then(() => {
-      console.log(
-        chalk.rgb(
-          255,
-          200,
-          255
-        )(`MongoDB connected to ${connection.db.databaseName}`)
-      );
-    });
-    dropCollection();
-    createCollection();
+    await mongoose.connect(MONGO_URL);
+    console.log(
+      chalk.rgb(
+        255,
+        200,
+        255
+      )(`MongoDB connected to ${connection.db.databaseName}`)
+    );
+    await dropCollection();
+    await createCollection();
   } catch (error) {
-    console.error(chalk.red(error));
+    console.error(chalk.red(`Can't connect to the databaseName: ${error.message}`));
   }
 };
 
 async function dropCollection() {
   try {
-    await connection.dropCollection("users").then(() => {
-      console.log(chalk.red(`Collection users dropped`));
-    });
+    await connection.dropCollection("users");
+    console.log(chalk.red(`Collection users dropped`));
   } catch (error) {
-    console.error(chalk.red(error));
+    console.error(chalk.red(error.message));
   }
 }
 async function createCollection() {
   try {
-    await connection.createCollection("users").then(() => {
-      console.log(chalk.blue(`Collection users created`));
-    });
+    await connection.createCollection("users");
+    console.log(chalk.blue(`Collection users created`));
   } catch (error) {
-    console.error(chalk.red(error));
+    console.error(chalk.red(error.message));
   }
 }
