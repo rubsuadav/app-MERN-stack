0c5a6b5b2481aab7ce4d48036e3f41b2e1284190
rubsuadav <rsuarezdavid@gmail.com>
2023-06-16 02:08:31 +0200

    upgrading the conection to mongodb for testing

--- server/src/db.js
+++ server/src/db.js
@@ -2,20 +2,41 @@ import mongoose from "mongoose";
 import { MONGO_URL } from "./config.js";
 import chalk from "chalk";
 
+const connection = mongoose.connection;
+
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(MONGO_URL).then(() => {
+      console.log(
+        chalk.rgb(
+          255,
+          200,
+          255
+        )(`MongoDB connected to ${connection.db.databaseName}`)
+      );
+    });
+    dropCollection();
+    createCollection();
   } catch (error) {
-    console.error(error);
+    console.error(chalk.red(error));
   }
 };
 
-mongoose.connection.on("connected", () => {
-  console.log(
-    chalk.rgb(
-      255,
-      200,
-      255
-    )(`MongoDB connected to ${mongoose.connection.db.databaseName}`)
-  );
-});
+async function dropCollection() {
+  try {
+    await connection.dropCollection("users").then(() => {
+      console.log(chalk.red(`Collection users dropped`));
+    });
+  } catch (error) {
+    console.error(chalk.red(error));
+  }
+}
+async function createCollection() {
+  try {
+    await connection.createCollection("users").then(() => {
+      console.log(chalk.blue(`Collection users created`));
+    });
+  } catch (error) {
+    console.error(chalk.red(error));
+  }
+}
