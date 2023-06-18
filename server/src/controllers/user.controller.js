4bec111227610ebb98786b0005f3bca3d77f5a82
rubsuadav <rsuarezdavid@gmail.com>
2023-06-18 03:26:36 +0200

    we can view teh user's profiles and update your own profile

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -4,6 +4,63 @@ import user from "../models/user.js";
 import { secret } from "../config.js";
 import { validateData, validateUniqueUser } from "../validators/validate.js";
 
+export const getUserProfile = async (req, res) => {
+  const { username } = req.params;
+  try {
+    const userByUsername = await user.find({ username });
+    const users = userByUsername.map((user) => user.username);
+    if (users.length === 0)
+      return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({
+      name: userByUsername[0].name,
+      username: userByUsername[0].username,
+      email: userByUsername[0].email,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateUserProfile = async (req, res) => {
+  const { username } = req.params;
+  const { firstName, lastName, email, password } = req.body;
+
+  const userId = req.userId;
+  const userByUsername = await user.find({ username });
+  const users = userByUsername.map((user) => user._id);
+  if (userId !== users[0].toString()) {
+    return res
+      .status(401)
+      .json({ message: "You can't upgrade one user profile if not yourself!" });
+  }
+  try {
+    let token = req.headers.authorization.split(" ")[1];
+    let updateData = { firstName, lastName, username, email };
+
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 12);
+      updateData.password = hashedPassword;
+      token = jwt.sign(
+        { email: result.email, username: result.username, id: result._id },
+        secret
+      );
+    }
+    const result = await user.findOneAndUpdate({ username }, updateData, {
+      new: true,
+    });
+
+    res.status(201).json({
+      username: result.username,
+      email: result.email,
+      name: result.name,
+      token,
+    });
+  } catch (error) {
+    res.status(409).json({ message: "Invalid data" });
+  }
+};
+
 export const signIn = async (req, res) => {
   const { email, password } = req.body;
   try {
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { signUp, signIn } from "../controllers/user.controller.js";
+import { signUp, signIn, getUserProfile, updateUserProfile } from "../controllers/user.controller.js";
+import { auth } from "../middlewares/auth.js";
 
 const router = Router();
 
+router.get("/users/:username", getUserProfile);
+
+router.patch("/users/:username", auth, updateUserProfile);
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 
